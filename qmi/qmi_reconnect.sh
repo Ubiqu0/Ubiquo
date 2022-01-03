afb0b021589fdec14c32eac57799876c19cae947
Miguel Won <miguelwon@gmail.com>
2022-01-03 11:05:47 +0000

    Update and add qmi

---
+++ qmi/qmi_reconnect.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+while true; do
+
+        ping -I wwan0 -c 1 8.8.8.8
+
+        if [ $? -eq 0 ]; then
+                echo "Connection up, reconnect not required..."
+        else
+                echo "Connection down, reconnecting..."
+                sudo .//home/pi/Documents/SIM8200-M2_5G_HAT_code/Goonline/simcom-cm
+        fi
+
+        sleep 1
+done
